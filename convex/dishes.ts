774c82d53baf012c531938b050483ffdcf49bfe3
Meet Batra <meetbatra56@gmail.com>
2026-06-24 14:49:50 +0530

    feat: populate db from Dish Assignment.json and update next config

--- convex/dishes.ts
+++ convex/dishes.ts
@@ -52,3 +52,48 @@ export const togglePublished = mutation({
   },
 });
 
+const newDishes = [
+  {
+    "dishName": "Jeera Rice",
+    "dishId": "1",
+    "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/jeera-rice.jpg",
+    "isPublished": true
+  },
+  {
+    "dishName": "Paneer Tikka",
+    "dishId": "2",
+    "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/paneer-tikka.jpg",
+    "isPublished": true
+  },
+  {
+    "dishName": "Rabdi",
+    "dishId": "3",
+    "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/rabdi.jpg",
+    "isPublished": true
+  },
+  {
+    "dishName": "Chicken Biryani",
+    "dishId": "4",
+    "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/chicken-biryani.jpg",
+    "isPublished": true
+  },
+  {
+    "dishName": "Alfredo Pasta",
+    "dishId": "5",
+    "imageUrl": "https://nosh-assignment.s3.ap-south-1.amazonaws.com/alfredo-pasta.jpg",
+    "isPublished": true
+  }
+];
+
+export const clearAndSeed = internalMutation({
+  args: {},
+  handler: async (ctx) => {
+    const existingDishes = await ctx.db.query("dishes").collect();
+    for (const dish of existingDishes) {
+      await ctx.db.delete(dish._id);
+    }
+    for (const dish of newDishes) {
+      await ctx.db.insert("dishes", dish);
+    }
+  },
+});
--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "nosh-assignment.s3.ap-south-1.amazonaws.com",
+      },
     ],
   },
 };
